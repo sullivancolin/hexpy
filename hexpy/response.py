3e49a61249d6ac07affc739dd7912274cb590a1b
Colin Sullivan <csullivan@crimsonhexagon.com>
2017-09-26 16:07:13 -0400

    removed print statement

--- hexpy/response.py
+++ hexpy/response.py
@@ -3,7 +3,6 @@
 
 
 def handle_response(response, check_text=False):
-    print(response)
     if response.status_code != 200:
         raise ValueError("Bad request." + response.text)
     if check_text:
