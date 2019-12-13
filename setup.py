b7bb5d48d4a15e27358ca64890a4005ec80e12b5
Colin Sullivan <csullivan@brandwatch.com>
2019-12-13 17:34:50 -0500

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md") as infile:
 
 setup(
     name="hexpy",
-    version="0.7.1",
+    version="0.7.2",
     description="Python Client for Crimson Hexagon API",
     long_description=long_description,
     long_description_content_type="text/markdown",
